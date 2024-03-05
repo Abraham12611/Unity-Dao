3e66ca542b24adf7ca3c5e0b46760c51d0f44d3e
Abraham12611 <abraham.dahunsi@gmail.com>
2024-03-05 15:30:42 +0100

    Initialized Blockchain function

--- blockchain.py
+++ blockchain.py
@@ -6,3 +6,12 @@ from uuid import uuid4
 
 import requests
 from flask import Flask, jsonify, request
+
+class Blockchain:
+    def __init__(self):
+        self.current_transactions = []
+        self.chain = []
+        self.nodes = set()
+
+        # Create the genesis block
+        self.new_block(previous_hash='1', proof=100)
