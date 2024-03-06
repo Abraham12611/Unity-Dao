5df5d1a10e87513952313354ee4d62ad602e33e0
Abraham12611 <abraham.dahunsi@gmail.com>
2024-03-06 19:12:40 +0100

    First property

--- blockchain.py
+++ blockchain.py
@@ -135,3 +135,19 @@ class Blockchain:
         })
 
         return self.last_block['index'] + 1
+
+     @property
+    def last_block(self):
+        return self.chain[-1]
+
+    @staticmethod
+    def hash(block):
+        """
+        Creates a SHA-256 hash of a Block
+
+        :param block: Block
+        """
+
+        # We must make sure that the Dictionary is Ordered, or we'll have inconsistent hashes
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
