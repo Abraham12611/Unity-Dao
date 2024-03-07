b801902921ce4eb6fb8eb6928f09213aaff691bd
Abraham12611 <abraham.dahunsi@gmail.com>
2024-03-07 22:38:45 +0100

    Instantiate the Blockchain

--- blockchain.py
+++ blockchain.py
@@ -194,3 +194,6 @@ app = Flask(__name__)
 
 # Generate a globally unique address for this node
 node_identifier = str(uuid4()).replace('-', '')
+
+# Instantiate the Blockchain
+blockchain = Blockchain()
