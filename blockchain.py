8bc699ff1e9f346a1debbcae7838370984080cfc
Abraham12611 <abraham.dahunsi@gmail.com>
2024-03-09 13:38:01 +0100

    created new app.route with function -- register_nodes -- Completed

--- blockchain.py
+++ blockchain.py
@@ -253,3 +253,16 @@ def full_chain():
 @app.route('/nodes/register', methods=['POST'])
 def register_nodes():
     values = request.get_json()
+
+     nodes = values.get('nodes')
+    if nodes is None:
+        return "Error: Please supply a valid list of nodes", 400
+
+    for node in nodes:
+        blockchain.register_node(node)
+
+    response = {
+        'message': 'New nodes have been added',
+        'total_nodes': list(blockchain.nodes),
+    }
+    return jsonify(response), 201
