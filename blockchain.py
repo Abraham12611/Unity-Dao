b969ef01160747c383f2ecb78026f7fb63b414e6
Abraham12611 <abraham.dahunsi@gmail.com>
2024-03-09 13:54:41 +0100

    created new app.route with function -- consesus -- completed

--- blockchain.py
+++ blockchain.py
@@ -270,3 +270,16 @@ def register_nodes():
 @app.route('/nodes/resolve', methods=['GET'])
 def consensus():
     replaced = blockchain.resolve_conflicts()
+
+    if replaced:
+        response = {
+            'message': 'Our chain was replaced',
+            'new_chain': blockchain.chain
+        }
+    else:
+        response = {
+            'message': 'Our chain is authoritative',
+            'chain': blockchain.chain
+        }
+
+    return jsonify(response), 200
