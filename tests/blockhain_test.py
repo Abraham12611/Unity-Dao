fbf6bb596e12d547471042119dec0b595bc4cdb9
Abraham12611 <abraham.dahunsi@gmail.com>
2024-03-10 08:37:41 +0100

    Insantiated blockhain test file -- added new class -- TestRegisterNodes -- added function -- test_idempotenncy

--- tests/blockhain_test.py
+++ tests/blockhain_test.py
@@ -34,3 +34,11 @@ class TestRegisterNodes(BlockchainTestCase):
         blockchain.register_node('http//192.168.0.1:5000')
 
         self.assertNotIn('192.168.0.1:5000', blockchain.nodes)
+
+        def test_idempotency(self):
+        blockchain = Blockchain()
+
+        blockchain.register_node('http://192.168.0.1:5000')
+        blockchain.register_node('http://192.168.0.1:5000')
+
+        assert len(blockchain.nodes) == 1
