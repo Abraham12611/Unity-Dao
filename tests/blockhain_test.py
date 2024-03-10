23f4f464456510df9f048f848238c2d86c9ee6c2
Abraham12611 <abraham.dahunsi@gmail.com>
2024-03-10 09:34:11 +0100

    Insantiated blockhain test file -- added new class -- TestBlocksAndTransaction

--- tests/blockhain_test.py
+++ tests/blockhain_test.py
@@ -42,3 +42,17 @@ class TestRegisterNodes(BlockchainTestCase):
         blockchain.register_node('http://192.168.0.1:5000')
 
         assert len(blockchain.nodes) == 1
+
+class TestBlocksAndTransactions(BlockchainTestCase):
+
+    def test_block_creation(self):
+        self.create_block()
+
+        latest_block = self.blockchain.last_block
+
+        # The genesis block is create at initialization, so the length should be 2
+        assert len(self.blockchain.chain) == 2
+        assert latest_block['index'] == 2
+        assert latest_block['timestamp'] is not None
+        assert latest_block['proof'] == 123
+        assert latest_block['previous_hash'] == 'abc'
