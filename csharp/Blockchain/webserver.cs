67dee7ed82b95252d1e530a2f464e46b30ed8fbb
Abraham12611 <abraham.dahunsi@gmail.com>
2024-03-11 08:35:51 +0100

    Initiated -- webserver.cs -- newspace

--- csharp/Blockchain/webserver.cs
+++ csharp/Blockchain/webserver.cs
@@ -3,3 +3,13 @@ using System.Configuration;
 using System.IO;
 using System.Net;
 using System.Net.Http;
+
+namespace BlockChainDemo
+{
+    public class WebServer
+    {
+        public WebServer(BlockChain chain)
+        {
+            var settings = ConfigurationManager.AppSettings;
+            string host = settings["host"]?.Length > 1 ? settings["host"] : "localhost";
+            string port = settings["port"]?.Length > 1 ? settings["port"] : "12345";
