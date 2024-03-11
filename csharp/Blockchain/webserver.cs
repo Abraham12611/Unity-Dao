b00a40c7f68a24c0b940196c1a14540a1539174c
Abraham12611 <abraham.dahunsi@gmail.com>
2024-03-11 08:54:24 +0100

    Initiated -- webserver.cs -- newspace -- finished fist part of function

--- csharp/Blockchain/webserver.cs
+++ csharp/Blockchain/webserver.cs
@@ -13,3 +13,16 @@ namespace BlockChainDemo
             var settings = ConfigurationManager.AppSettings;
             string host = settings["host"]?.Length > 1 ? settings["host"] : "localhost";
             string port = settings["port"]?.Length > 1 ? settings["port"] : "12345";
+
+            var server = new TinyWebServer.WebServer(request =>
+                {
+                    string path = request.Url.PathAndQuery.ToLower();
+                    string query = "";
+                    string json = "";
+                    if (path.Contains("?"))
+                    {
+                        string[] parts = path.Split('?');
+                        path = parts[0];
+                        query = parts[1];
+                    }
+
