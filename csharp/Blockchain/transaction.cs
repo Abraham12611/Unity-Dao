1ef10908abda9c333ec40305f06e28ab5028745b
Abraham12611 <abraham.dahunsi@gmail.com>
2024-03-15 11:43:19 +0100

    initiated -- transaction.cs -- finalised

--- csharp/Blockchain/transaction.cs
+++ csharp/Blockchain/transaction.cs
@@ -2,3 +2,8 @@ namespace BlockChainDemo
 {
     public class Transaction
     {
+        public int Amount { get; set; }
+        public string Recipient { get; set; }
+        public string Sender { get; set; }
+    }
+}
