4927bc8d4422f05a13f8b0a6e457bf704d0514e5
Abraham12611 <abraham.dahunsi@gmail.com>
2024-03-16 06:23:18 +0100

    initiated -- blockchain.cs -- creayed new namespace

--- csharp/Blockchain/blockchain.cs
+++ csharp/Blockchain/blockchain.cs
@@ -7,3 +7,14 @@ using System.Linq;
 using System.Net;
 using System.Security.Cryptography;
 using System.Text;
+
+namespace BlockChainDemo
+{
+    public class BlockChain
+    {
+        private List<Transaction> _currentTransactions = new List<Transaction>();
+        private List<Block> _chain = new List<Block>();
+        private List<Node> _nodes = new List<Node>();
+        private Block _lastBlock => _chain.Last();
+
+        public string NodeId { get; private set; }
