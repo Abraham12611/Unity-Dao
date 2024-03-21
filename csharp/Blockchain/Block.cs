91f0dda3c38daeceea6622fe5fd7a15771135d1e
Abraham12611 <abraham.dahunsi@gmail.com>
2024-03-21 07:19:12 +0100

    instantiated -- block.cs -- started new function

--- csharp/Blockchain/Block.cs
+++ csharp/Blockchain/Block.cs
@@ -1,2 +1,12 @@
 using System;
 using System.Collections.Generic;
+
+namespace BlockChainDemo
+{
+    public class Block
+    {
+        public int Index { get; set; }
+        public DateTime Timestamp { get; set; }
+        public List<Transaction> Transactions { get; set; }
+        public int Proof { get; set; }
+        public string PreviousHash { get; set; }
