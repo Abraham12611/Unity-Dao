125a44618d2d3abb19c42dcd57b79f631faae7af
Abraham12611 <abraham.dahunsi@gmail.com>
2024-03-21 07:39:48 +0100

    instantiated -- block.cs -- started new function -- closed

--- csharp/Blockchain/Block.cs
+++ csharp/Blockchain/Block.cs
@@ -10,3 +10,10 @@ namespace BlockChainDemo
         public List<Transaction> Transactions { get; set; }
         public int Proof { get; set; }
         public string PreviousHash { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Index} [{Timestamp.ToString("yyyy-MM-dd HH:mm:ss")}] Proof: {Proof} | PrevHash: {PreviousHash} | Trx: {Transactions.Count}";
+        }
+    }
+}
