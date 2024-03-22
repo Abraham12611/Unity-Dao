b24c1728a5970c7980fe1cc46f63a87e7325e272
Abraham12611 <abraham.dahunsi@gmail.com>
2024-03-22 11:29:28 +0100

    initiated -- assemblyinfo.cs -- set guid value

--- csharp/Blockchain/Properties/AssemblyInfo.cs
+++ csharp/Blockchain/Properties/AssemblyInfo.cs
@@ -18,3 +18,7 @@ using System.Runtime.InteropServices;
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e06fc4ce-77d0-4a64-94a6-32a08920e481")]
+
