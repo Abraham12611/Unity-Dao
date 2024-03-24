8f3209b3e1a610c85410bb2d48aa3b024b2028f9
Abraham12611 <abraham.dahunsi@gmail.com>
2024-03-24 07:43:59 +0100

    created new file -- blockchain.js -- instantiated -- set new class Blockchain

--- js/blockchain.js
+++ js/blockchain.js
@@ -1,1 +1,9 @@
 const crypto = require("crypto");
+
+class Blockchain {
+    constructor() {
+        this.chain = [];
+        this.pendingTransactions = [];
+        this.newBlock();
+        this.peers = new Set();
+    }
